fc9495db8050c521eb23fa85aee48de27b9713ad
allonsg <109518407+allonsg@users.noreply.github.com>
2023-05-16 18:39:27 +0300

    created user router and fixed all imports bug

--- server/src/controllers/media.controller.js
+++ server/src/controllers/media.controller.js
@@ -1,9 +1,9 @@
-import responseHandler from "../handlers/response.handler";
+import responseHandler from "../handlers/response.handler.js";
 import favoriteModel from "../models/favorite.model.js";
 import tmdbApi from "../tmdb/tmdb.api.js";
-import userModel from "../models/user.model";
+import userModel from "../models/user.model.js";
 import reviewModel from "../models/review.model.js";
-import tokenMiddleware from "../middlewares/token.middleware";
+import tokenMiddleware from "../middlewares/token.middleware.js";
 
 const getList = async (req, res) => {
   try {
