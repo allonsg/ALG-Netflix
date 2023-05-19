0b57a53090307594616b06f7f2cdd49eb74a41e8
allonsg <109518407+allonsg@users.noreply.github.com>
2023-05-19 23:31:41 +0300

    created movie reviews block

--- client/src/pages/MediaDetail.jsx
+++ client/src/pages/MediaDetail.jsx
@@ -18,6 +18,7 @@ import BackdropSlide from "../components/common/BackdropSlide";
 import PosterSlide from "../components/common/PosterSlide";
 import RecommendSlide from "../components/common/RecommendSlide";
 import MediaSlide from "../components/common/MediaSlide";
+import MediaReview from "../components/common/MediaReview";
 
 import uiConfigs from "../configs/ui.config";
 import tmdbConfigs from "../api/config/tmdb.configs";
@@ -285,6 +286,11 @@ const MediaDetail = () => {
         {/* media posters */}
 
         {/* media reviews */}
+        <MediaReview
+          reviews={media.reviews}
+          media={media}
+          mediaType={mediaType}
+        />
         {/* media reviews */}
 
         {/* media recommendations */}
